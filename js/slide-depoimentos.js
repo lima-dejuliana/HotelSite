f4c8986596b85492ea103cd54d32d0ff6043b812
lima-dejuliana <lima-dejuliana@users.noreply.github.com>
2018-04-10 15:15:11 -0300

    slide depoimentos

---
+++ js/slide-depoimentos.js
@@ -0,0 +1,21 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides-depo");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 3,5 seconds
+}
